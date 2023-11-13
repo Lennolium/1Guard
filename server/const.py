c737e79d0372287ed22d8f11f920aa7c883a5eb8
Lennolium <lennart-haack@mail.de>
2023-11-13 13:32:20 +0100

    Vercel fix

--- server/const.py
+++ server/const.py
@@ -22,6 +22,7 @@ import platform
 # Constants.
 CURRENT_PLATFORM = platform.uname()[0].upper()  # 'DARWIN' / 'LINUX' ...
 APP_PATH = os.path.dirname(os.path.abspath(os.path.realpath(__file__)))
-LOG_FILE = f"{APP_PATH}/tmp/server.log"
+# LOG_FILE = f"{APP_PATH}/log/server.log"
+LOG_FILE = "/tmp/server.log"
 
 # Database.
